abf6155ff86b55c7a48725c5a953f79f0c59cd00
naoyashiga <naoyashiga@MacBook-Air.local>
2017-07-09 10:45:12 +0900

    refactor

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # Locus
 
 ![locus](https://user-images.githubusercontent.com/1988660/27990421-b7d6b1f4-6491-11e7-80b0-dc5dabceb555.gif)
+
+## Javascript and Paperscript
+
+```html
+<script type="text/javascript" src="js/init.js"></script>
+<script type="text/javascript" src="js/paper-full.min.js"></script>
+<script type="text/paperscript" src="js/app.js" canvas="myCanvas"></script>
+```
+
+I used exclude files, js and paperscript.
+
+If you don't know about paperscript, please check below link.
+[Paper.js — Working with Paper.js](http://paperjs.org/tutorials/getting-started/working-with-paper-js/)
+
+## Loading SVG
+
+In init.js, I define square SVG path.
+
+```js
+var pathSize = 400;
+var pathData = "M10,10h" + pathSize + "v" + pathSize + "h" + "-" + pathSize + "z"
+```
+
+## Convert SVG to Path
+
+```js
+var svgPath = new Path(pathData);
+svgPath.strokeColor = "#364F6B";
+svgPath.strokeWidth = 2;
+svgPath.position = view.center;
+```
+
+
+
+
+# Thanks
+
+- [Paper.js](http://paperjs.org/)
--- src/js/app.js
+++ src/js/app.js
@@ -8,23 +8,21 @@ function Ball(point, speed){
 }
 
 function MidPoint(point){
-    this.path = new Path.Circle({
-        center:point,
-        radius:10,
-        fillColor: "#FC5185"
-    });
+	this.path = new Path.Circle({
+		center:point,
+		radius:10,
+		fillColor: "#FC5185"
+	});
 }
 
-function Trajectory(point){
-    this.path = new Path({
-			segments: [point],
-			strokeColor: "#FC5185",
-			strokeWidth: 3
-			// Select the path, so we can see its segment points:
-			// fullySelected: true
-		});
+function Locus(point){
+	this.path = new Path({
+		segments: [point],
+		strokeColor: "#FC5185",
+		strokeWidth: 3
+	});
 
-		this.path.simplify(10);
+	this.path.simplify(10);
 }
 
 function MyLine(from, to){
@@ -39,17 +37,14 @@ function MyLine(from, to){
 var balls = [],
     numBalls = 2;
 
+var svgPath = new Path(pathData);
+svgPath.strokeColor = "#364F6B";
+svgPath.strokeWidth = 2;
+svgPath.position = view.center;
+// svgPath.scale(0.5);
+// svgPath.rotate(-180)
 
-
-var poop = new Path(pathData);
-poop.strokeColor = "#364F6B";
-poop.strokeWidth = 2;
-poop.position = view.center;
-poop.scale(0.5);
-poop.rotate(-180)
-
-var startPoint = poop.getPointAt(0)
-
+var startPoint = svgPath.getPointAt(0)
 
 for(var j = 0;j < numBalls;j++){
 	// balls.push(new Ball(startPoint, 0.1 * (j*0.8 + 0.5)));
@@ -57,31 +52,30 @@ for(var j = 0;j < numBalls;j++){
 }
 
 var midPoint = new MidPoint(startPoint);
-// var line = new MyLine(new Point(0, 0), startPoint);
 var myLine = new MyLine(startPoint, startPoint);
-var trajectory = new Trajectory(startPoint, startPoint);
+var locus = new Locus(startPoint, startPoint);
 
-console.log(myLine);
 
 function onFrame(event){
 	var offset, point;
 	for(var i = 0;i < numBalls;i++){
-	    offset = poop.length * (event.count * balls[i].speed  % 100) / 100;
+		offset = svgPath.length * (event.count * balls[i].speed  % 100) / 100;
 
-	    point = poop.getPointAt(offset);
+		point = svgPath.getPointAt(offset);
 
-	    balls[i].path.position = point;
+		balls[i].path.position = point;
 	}
 
 	var p1 = balls[0].path.position;
 	var p2 = balls[1].path.position;
-	midPoint.path.position = (p1 + p2) / 2;
 
-	if(offset < poop.length) {
-		trajectory.path.add(midPoint.path.position);
-	}
+	midPoint.path.position = (p1 + p2) / 2;
 
 	myLine.path.remove();
 	myLine = new MyLine(p1, p2);
-	myLine.path.insertAbove(poop)
+	myLine.path.insertAbove(svgPath)
+
+	if(offset < svgPath.length) {
+		locus.path.add(midPoint.path.position);
+	}
 }
